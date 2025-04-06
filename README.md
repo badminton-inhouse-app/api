7aaaf149c07cc82ceca1d8aff50bdf8c4f1ac965
lehoangvuvt <hoangvule100@gmail.com>
2025-04-06 14:10:39 +0700

    feat: setup project, database with Drizzle

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# api
+## Migration with Drizzle
+
+- Running `pnpm  db-migrate` to run the database migration.
+- Running `pnpm db-gen` to generate database migration sql files (migration files should be in root/drizzle folder).
---
+++ src/database/schema/index.ts
@@ -0,0 +1,58 @@
+import {
+  pgTable,
+  uuid,
+  text,
+  timestamp,
+  pgEnum,
+  integer,
+} from 'drizzle-orm/pg-core';
+
+export const genderEnum = pgEnum('gender', ['MALE', 'FEMALE']);
+export const experienceEnum = pgEnum('experience', [
+  'U_1_M', // 0-1 month
+  'U_3_M', // 0-3 months
+  'U_6_M', // 3-6 months
+  'U_1_YEAR', // 6-12 months
+  'U_2_YEARS', // 1-2 years
+  'U_3_YEARS', // 2-3 years
+  'O_3_YEARS', // 3+ years
+]);
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at'),
+});
+
+export const user_profiles = pgTable('user_profiles', {
+  id: uuid('id').primaryKey(),
+  name: text('name').notNull(),
+  userId: uuid('user_id').references(() => users.id),
+  gender: genderEnum('gender').notNull().default('MALE'),
+  phoneNo: text('phone'),
+  avatar: text('avatar'),
+  birthday: timestamp('birthday'),
+  experience: experienceEnum('experience').notNull().default('U_1_M'),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at'),
+});
+
+export const centers = pgTable('centers', {
+  id: uuid('id').primaryKey(),
+  address: text('address').notNull().default(''),
+  district: text('district').notNull().default(''),
+  city: text('city').notNull().default(''),
+  phoneNo: text('phone_no').notNull().default(''),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at'),
+});
+
+export const courts = pgTable('courts', {
+  id: uuid('id').primaryKey(),
+  centerId: uuid('center_id').references(() => centers.id),
+  courtNo: integer('court_no').notNull().unique(),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at'),
+});
